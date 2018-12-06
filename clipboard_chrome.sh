718946d60ffbc0596f342bbe3930fd417229843b
atomixhawk <githinmanesh@gmail.com>
2018-12-06 14:38:08 +0530

    Search/Browse Keyword/URL in Clipboard

---
+++ clipboard_chrome.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+regex='(https?|ftp|file)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]'
+string="$(xclip -o)"
+if [[ $string =~ $regex ]]  		#check if text in clipboard is a valid URL
+then 
+    google-chrome-stable "$(xclip -o)"
+else								#else google search for text in clipboard 	
+    google-chrome-stable https://www.google.com/search?q="${string/\&/%26}"
+fi
+
+# first do install xclip (do: pacman -S xclip)	
+# I saved this script in a new folder .scripts in Home
+# and added keyboard shortcut in settings for the command:
+# bash /home/githin/.scripts/clipboard_chrome.sh
+# Now when i press this keyboard shortcut, if the text in clipboard is a valid URL,
+# then it is opened in chrome, else does a google search for text in clipboard.	
