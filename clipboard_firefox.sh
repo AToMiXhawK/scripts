7f7e35f89befdbdad4687973cd78ec30a43c81dd
AToMiXhawK <githinmanesh@gmail.com>
2019-01-24 23:39:55 +0530

    Added comments

--- clipboard_firefox.sh
+++ clipboard_firefox.sh
@@ -7,3 +7,10 @@ then
 else
 	firefox https://duckduckgo.com/?q="${string/\&/%26}"
 fi
+
+# first do install xclip (do: pacman -S xclip)	
+# I saved this script in a new folder .scripts in Home
+# and added keyboard shortcut in settings for the command:
+# bash /home/githin/.scripts/clipboard_chrome.sh
+# Now when i press this keyboard shortcut, if the text in clipboard is a valid URL,
+# then it is opened in chrome, else does a google search for text in clipboard.	
