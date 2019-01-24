58741fcaf541a1ae48ad71c1f9f24aaaab0ae704
AToMiXhawK <githinmanesh@gmail.com>
2019-01-24 23:01:43 +0530

    fixed errors

--- bumblebee/bee.sh
+++ bumblebee/bee.sh
@@ -3,11 +3,11 @@
 x=`pacman -Qs bumblebee`
 if [ -n "$x" ]
 then 
-	zenity --width=500 --question --title="Hawk Gfx Switch" echo --text="Graphics Card is cureently DISABLED, Do you want to turn it on?"
+	zenity --width=500 --question --title="Hawk Gfx Switch" echo --text="Graphics Card is cureently DISABLED, Do you want to turn it on?\nThis will disable Power Saving.\nConnect to External power source."
   	if [ $? = 0 ]
 	then
-		gksu "pacman -U bumblebee-3.2.1-20-x86_64.pkg.tar.xz --noconfirm"
-		gksu "systemctl enable bumblebeed"
+		gksu "systemctl disable bumblebeed"
+		gksu "pacman -R bumblebee --noconfirm"
 		zenity --question --width=500 --title="Hawk Gfx Switch" echo --text="Reboot now?"
 		if [ $? = 0 ]
 		then	
@@ -22,8 +22,8 @@ else
 	zenity --question --width=500 --title="Hawk Gfx Switch" echo --text="Graphics Card is cureently ENABLED, Do you want to turn it off?"
 	if [ $? = 0 ]
 	then
-		gksu "systemctl disable bumblebeed"
-		gksu "pacman -R bumblebee --noconfirm"
+		gksu "pacman -U bumblebee-3.2.1-20-x86_64.pkg.tar.xz --noconfirm"
+		gksu "systemctl enable bumblebeed"
 		zenity --question --width=500 --title="Hawk Gfx Switch" echo --text="Reboot now?"
 		if [ $? = 0 ]
 		then
