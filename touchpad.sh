6cecbf231dce1872f3201f5e8591274f2c9c3740
atomixhawk@gmail.com <hawk@phoenix.localdomain>
2019-01-24 12:12:49 +0530

    Change touchpad settings for ArchLinux

---
+++ touchpad.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+synclient VertScrollDelta=-78
+synclient HorizScrollDelta=-78
+synclient TapButton1=1
+synclient TapButton2=3
+synclient TapButton3=2
+PalmDetect=1
+synclient VertEdgeScroll=1
+synclient HorizEdgeScroll=1
