c21d239441a0b76e2bdb0010300810fb14b09781
AToMiXhawK <githinmanesh@gmail.com>
2019-01-27 19:26:31 +0530

    Script to send error log to tg and hastebin

---
+++ rombuild/buildfailed.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+line=$(grep -n -m 1 "FAILED:" build.log | cut -f1 -d:)
+line=`expr $line - 1`
+tail --lines=+$line build.log >> error.log
+telegram-send $(cat error.log | haste) -f error.log
